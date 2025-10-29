db86684fb8a5f5a73ca63c05d943b5dd8396cd6c
Eduardo Luís de Andrade Santos <eduardo775445@gmail.com>
2025-10-29 15:47:28 -0300

    feat(passwd_gen): generate passwords and validate format of input

---
+++ core/passwd_gen.py
@@ -0,0 +1,27 @@
+import re
+
+def validate_format(user_name: str) -> bool:
+    std = r'^[A-Za-zÀ-ÖØ-öø-ÿ]+\.[A-Za-zÀ-ÖØ-öø-ÿ]+$'
+    return bool(re.match(std, user_name))
+
+def generate_password(user_name) -> str:
+    if not validate_format(user_name):
+        raise ValueError("Formato inválido! \nUse o formato: 'nome.sobrenome'")
+
+    parts = user_name.split(".")
+    first = parts[0][0].upper()
+    first += parts[0][1]
+    second = parts[1][0].capitalize()
+    return f"{first}{second}@work2025"
+''' Try Except 
+try:
+    user_name = input("Digite o nome (formato nome.sobrenome): ").strip()
+    password = generate_password(user_name)
+    print(f"Senha gerada: {password}")
+    
+except ValueError as err:
+    print(f"Erro: {err}")
+    
+except Exception as err:
+    print(f"Erro inesperado: {err}")
+'''
