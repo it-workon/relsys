7b6437c4c46a2485aa413d8e589c6102c7b575ad
Eduardo Luís de Andrade Santos <eduardo775445@gmail.com>
2025-11-21 13:47:27 -0300

    fix: merge conflicts

--- core/desligamento.py
+++ core/desligamento.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import messagebox
 from openpyxl import load_workbook
 
 CAMINHO_PLANILHA = r"Z:\NEW TECNOLOGIA\1 Documentação\2 Inventário\6 Colaboradores Desligados\Colaboradores desligados.xlsx"
