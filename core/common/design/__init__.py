15aee3dc8bae4707d55a816c73a75fe3954c7438
Eduardo Luís de Andrade Santos <eduardo775445@gmail.com>
2025-12-16 14:10:50 -0300

    refactor: design system - adding __init__ file

---
+++ core/common/design/__init__.py
@@ -0,0 +1,11 @@
+from base import Design
+from Colors import Colors
+from Padding import Padding
+from Typography import Typography
+from Components import Components
+
+# Injeta como atributos do Design (namespace)
+Design.Colors = Colors
+Design.Padding = Padding
+Design.Typography = Typography
+Design.Components = Components
